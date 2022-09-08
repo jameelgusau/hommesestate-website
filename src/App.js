1113beefd5019e25de5aecf217f94c77dfee9b35
jameelgusau <jameelgusau@gmail.com>
2022-09-08 14:03:48 +0100

    change routings adding loader and notfound page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import React, { lazy, Suspense } from "react";
-import {
-  Route,
-  Routes,
-  Navigate,
-  BrowserRouter as Router,
-} from "react-router-dom";
+import { Route, Routes, Navigate, } from "react-router-dom";
 import Home from "./components/home";
 import LandingLayout from "./components/LandingLayout";
 // import Properties from "./components/pages/properties";
 import PropertyLayout from "./components/PropertyLayout";
-import Loading from "./components/pages/Loading";
+import Loading from "./components/pages/Loading"
 
-// const NotFound = lazy(() => import("./components/pages/notfound"));
-const Properties = lazy(() => import("./components/pages/properties"));
+const NotFound = lazy(() => import("./components/pages/notfound"));
+const Properties = lazy(() => import("./components/pages/properties"))
 
 function App() {
   return (
-    <Router>
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route element={<LandingLayout />}>
-            <Route  path="/" element={<Home />} />
-          </Route>
-          <Route path="property" element={<PropertyLayout />}>
-            <Route path=":id" element={<Properties />} />
-          </Route>
-          {/* <Route path="notfound" element={<NotFound />} /> */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <Suspense fallback={<Loading />}>
+    <Routes>
+      <Route path="/" element={<LandingLayout />}>
+        <Route index element={<Home />} />
+        {/* <Route path="/property" element={<Properties />} /> */}
+      </Route>
+      <Route path="property" element={<PropertyLayout />}>
+        <Route path=":id"  element={<Properties  />} />
+      </Route>
+      <Route path="notfound" element={<NotFound />} />
+      <Route path="*" element={<Navigate to="/notfound" replace />} />
+    </Routes>
+    </Suspense>
   );
 }
 
